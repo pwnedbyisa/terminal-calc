cc073a32a1b2159caa0e150e25707eb932a31709
Isabelle <138353745+pwnedbyisa@users.noreply.github.com>
2023-08-19 19:12:36 -0500

    Create calcbanners.py

---
+++ calcbanners.py
@@ -0,0 +1,22 @@
+# banner v1.1
+print('\n')
+# raw bc the second line wasn't printing for wtv reason
+print(r' .d8888b.        d8888 888      .d8888b.       888     888  d888        d888')
+print('d88P  Y88b      d88888 888     d88P  Y88b      888     888 d8888       d8888')
+print(r'888    888     d88P888 888     888    888      888     888   888         888')
+print(r'888           d88P 888 888     888             Y88b   d88P   888         888')
+print(r'888          d88P  888 888     888              Y88b d88P    888         888')
+print(r'888    888  d88P   888 888     888    888        Y88o88P     888         888')
+print(r'Y88b  d88P d8888888888 888     Y88b  d88P         Y888P      888   d8b   888')
+print(r' "Y8888P" d88P     888 88888888 "Y8888P"           Y8P     8888888 Y8P 8888888 ')
+print('\n\t    Welcome to Calc CLI! Input -h or --help for options <3\n' + '=' * 79 + '\n')
+
+# banner v1.2
+print('\n')
+print(' ██████╗ █████╗ ██╗      ██████╗    ██╗   ██╗ ██╗   ██████╗')
+print('██╔════╝██╔══██╗██║     ██╔════╝    ██║   ██║███║   ╚════██╗')
+print('██║     ███████║██║     ██║         ██║   ██║╚██║    █████╔╝')
+print('██║     ██╔══██║██║     ██║         ╚██╗ ██╔╝ ██║   ██╔═══╝')
+print('╚██████╗██║  ██║███████╗╚██████╗     ╚████╔╝  ██║██╗███████╗')
+print(' ╚═════╝╚═╝  ╚═╝╚══════╝ ╚═════╝      ╚═══╝   ╚═╝╚═╝╚══════╝')
+print('\n   Welcome to Calc CLI! Input -h or --help for options <3\n' + '=' * 60 + '\n')
